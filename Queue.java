3296c3572acc096c3b63f740d6a4fe289a16c08f
Brayan Monsalvo <brayanmonsalvo4@gmail.com>
2022-09-13 00:06:08 -0500

    Queue.java con metodos iniciales agregado

---
+++ Queue.java
@@ -0,0 +1,60 @@
+public class Queue <E> {
+    private Node<E> head;
+    private int numElements = 0;
+
+    public Queue(){
+
+    }
+
+    public Queue (E head){
+        this.head = new Node <E>(head);
+        numElements = (head == null) ? 0 : 1;
+    }
+
+    public void add(E element){
+        //si la cola esta vacia, entonces el elemento que se quiere ingresar sera la nueva
+        //cabeza de la cola
+
+        if(element == null){
+            return;
+        }
+
+        if(isEmpty()){
+            head = new Node <E> (element);
+            numElements++;
+
+            return;
+        }
+
+        //se recorrera toda la cola hasta llegar al ultimo nodo, para ello se crea
+        //un nodo temporal que apuntara inicialmente a la cabeza
+        Node <E> temp = head;
+
+        do{
+            temp = temp.getNextNode();
+        }while(temp.hasNextNode());
+
+        temp.setNextNode(new Node(element));
+        numElements++;
+    }
+
+    public E element(){
+        return head.getElement();
+    }
+
+    public E poll(){
+        Node <E> h = head;
+
+        head.setNextNode( head.getNextNode() );
+
+        return h.getElement();
+    }
+
+    public boolean isEmpty(){
+        return (numElements == 0);
+    }
+
+    public int getNumberElements(){
+        return numElements;
+    }
+}
